aa68d6915f1b649542288747c3b8c493dcedf0f1
Stanislav Chren <stanislavch@gmail.com>
2017-03-30 02:49:34 +0200

    init version

---
+++ src/main/java/pv260/unittesting/Sorter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pv260.unittesting;
+
+import java.util.List;
+
+/**
+ *
+ * @author stano
+ */
+   public interface Sorter {
+
+        List<Integer> sort(List<Integer> arr);
+
+    }
